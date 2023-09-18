7ba298b14c25bf50ff0adbc45d4c705503c663b9
kartik yadav <114292174+yadavkartik180@users.noreply.github.com>
2023-09-18 07:53:03 +0530

    Add files via upload

---
+++ calculator/script.js
@@ -0,0 +1,9 @@
+function setvalv(value) {
+  var result = document.getElementById("result");
+  result = result.value += value;
+}
+function clearScreen(value) {
+  var clear = document.getElementById("ac");
+  var result = document.getElementById("result");
+  result = result.value = "";
+}
